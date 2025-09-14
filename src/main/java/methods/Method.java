3ee1fa3570a6b7b5dc96af0df5dea645cabe4973
sameer <shaik.sameer.ahamad.-@ibm.com>
2025-09-14 19:45:00 +0530

    added 2 new lines

--- src/main/java/methods/Method.java
+++ src/main/java/methods/Method.java
@@ -7,6 +7,8 @@ package methods;
 public class Method {
 	  static void myMethod() {
 	    System.out.println("I just got executed!");
+	    System.out.println("Call me from main");
+	    System.out.println("Iam waiting for ALLAH");
 	  }
 
 	  public static void main(String[] args) {
