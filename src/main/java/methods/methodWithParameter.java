439382baf84f51dd2d3ed0ef3228115952699eae
sameer <shaik.sameer.ahamad.-@ibm.com>
2025-09-14 16:40:07 +0530

    my first commit

---
+++ src/main/java/methods/methodWithParameter.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class methodWithParameter {
+        static void myMethod(String fname) {
+		    System.out.println(fname + " Refsnes");
+		  }
+
+		  public static void main(String[] args) {
+		    myMethod("Liam");
+		    myMethod("Jenny");
+		    myMethod("Anja");
+		  }
+		}
+
